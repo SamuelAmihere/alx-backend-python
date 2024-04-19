1677a41011d6340244940b6972003666a30c7f9b
Samuel Amihere <amisam2000@gmail.com>
2024-04-19 13:48:33 +0800

    Concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,5 +3,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """Return the concatenation of two strings"""
+    """Return the concatenation of two strings"""ncatenates two strings.
     return str1 + str2
