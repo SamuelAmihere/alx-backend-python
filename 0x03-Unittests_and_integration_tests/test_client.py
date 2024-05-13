29a98034d9d7528e9e3981afe5a03ae5fdb72d19
Samuel Amihere <amisam2000@gmail.com>
2024-05-13 20:03:06 +0800

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,3 +21,14 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(response, {"payload": True})
         mock_get.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that GithubOrgClient._public_repos_url
+        returns the correct value.
+        """
+        payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = payload
+
+        test_client = GithubOrgClient('google')
+        self.assertEqual(test_client._public_repos_url, payload["repos_url"])
