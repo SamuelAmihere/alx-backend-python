5818956668503172ad9dd76cb7398a72085996d3
Samuel Amihere <amisam2000@gmail.com>
2024-05-13 22:08:40 +0800

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -89,8 +89,8 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.assert_called_once()
 
     @parameterized.expand([
-    ({"license": {"key": "my_license"}}, "my_license", True),
-    ({"license": {"key": "other_license"}}, "my_license", False),
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
     ])
     def test_has_license(self, repo, license_key, expected_result):
         """Test GithubOrgClient.has_license."""
